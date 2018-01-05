e4fce66f7598819586993d21fc0e0e0e4f6cba6c
Katie <kathleen.bennett@utah.edu>
2018-01-05 02:59:20 +0000

    must have changed these

--- modules/m8-modules/ClassPrep/chapmanbe/__init__.py
+++ modules/m8-modules/ClassPrep/chapmanbe/__init__.py
@@ -1,10 +1,16 @@
 """This is the __init__ file for the chapmanbe package"""
 
-__all__ = ["my_favorite_functions"]
+__all__ = ["my_favorite_functions", "hello"]
 from . import my_favorite_functions
 import getpass
 import os
 creator_name = "Brian Chapman"
 your_name = getpass.getuser()
 
+#try:
+#    os.mkdir(os.path.join(os.path.expanduser("~"), ".chapmanbe_data"))
+#except: FileExistsError
+#    pass
 
+if not os.path.exists(os.path.join(os.path.expanduser("~"), ".chapmanbe_data")):
+                      os.mkdir(os.path.join(os.path.expanduser("~"), ".chapmanbe_data"))
